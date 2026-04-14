65f3a4cb961e60f71b4d562b2cfe76a78a455496
Alcartez <67427418+Alcartez@users.noreply.github.com>
2026-04-14 16:00:57 +0530

    Update environment file for GROMACS installation

--- install_conda_kaggle.sh
+++ install_conda_kaggle.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+
 set -e 
 
 echo "Installing Micromamba..."
+
 curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+
 mv bin/micromamba /usr/local/bin/micromamba
 
 export MAMBA_ROOT_PREFIX=/opt/micromamba
 
 # --- THE FIX FOR THE CUDA ERROR ---
+
 # This forces the solver to recognize CUDA 11.8 exists on the Kaggle machine
+
 export CONDA_OVERRIDE_CUDA="11.8"
 
 echo "Creating GROMACS environment..."
-micromamba create -y -f environment.yml
 
-echo "DONE."
+micromamba create -y -f kaggle_environment.yml
